bca6539c36536be3e5122764222c6000a12e3d1a
David Cantú <32000336+codyvi@users.noreply.github.com>
2019-11-24 19:53:05 -0600

    Finished Scripts for level 1

---
+++ Scripts/Choque.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Choque : MonoBehaviour
+{
+
+    public Text Txt;
+
+    public static int llavesRecogidas;
+    public bool perdiste;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Txt.text = "Llaves: ";
+        llavesRecogidas = 0;
+        perdiste = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Txt.text = "Llaves: " + llavesRecogidas;
+
+        if(perdiste)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            SceneManager.LoadScene(2);
+        }
+
+    }
+    
+    void OnCollisionEnter (Collision col)
+    {
+        if(col.gameObject.name == "llave1" || col.gameObject.name == "llave2" || col.gameObject.name == "llave3")
+        {
+            llavesRecogidas++;
+            Destroy(col.gameObject);
+        }
+
+        //Dejar asi temporalmente, cambiar cuando esten los niveles 
+        if( (col.gameObject.name == "Puerta1" || col.gameObject.name == "Puerta2" || col.gameObject.name == "Puerta3" ) && llavesRecogidas == 3)
+        {
+            perdiste = true;
+        }
+    }
+}
---
+++ Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Jugar()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Instrucciones()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Terminar()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+   
+}
